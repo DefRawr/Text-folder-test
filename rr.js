16600987ee2ec03eef5438622bbff656d7b28fb5
Dejan <d.bancevic@metalfer.net>
2023-01-09 13:36:16 +0100

    nez

--- rr.js
+++ rr.js
@@ -1,1 +1,15 @@
-console.log('yo')
+console.log('yoaaaaaaaaa');
+
+
+
+// store input numbers
+const num1 = parseInt(prompt('Enter the first number '));
+const num2 = parseInt(prompt('Enter the second number '));
+
+//add two numbers
+const sum = num1 + num2;
+
+// display the sum
+console.log(`The sum of ${num1} and ${num2} is ${sum}`);
+
+
